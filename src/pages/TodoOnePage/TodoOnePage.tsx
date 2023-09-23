39d88c75e58cea232b127fb112d7d8b2bdef732e
Asadullo <asadullon768@gmail.com>
2023-09-24 03:02:49 +0500

    I have finished my task in todo_list

--- src/pages/TodoOnePage/TodoOnePage.tsx
+++ src/pages/TodoOnePage/TodoOnePage.tsx
@@ -5,12 +5,14 @@ import {
   FieldType,
   TypedField,
   usePreventLeave,
+  sleep,
 } from "react-declarative";
 
 import fetchApi from "../../helpers/fetchApi";
 import history from "../../helpers/history";
 
 import ITodoItem from "../../model/ITodoItem";
+import { Email } from "@mui/icons-material";
 
 interface ITodoOnePageProps {
   id: string;
@@ -20,28 +22,191 @@ const fields: TypedField[] = [
   {
     type: FieldType.Div,
     style: {
-      padding: "10px",
+      padding: "5px 50px",
       display: "grid",
-      gridTemplateColumns: "150px auto",
+      gridTemplateColumns: "200px auto",
       gap: "5px",
     },
     fields: [
-      // /images
-
       {
         type: FieldType.Group,
+        // /images
         fields: [
           {
             type: FieldType.Box,
             style: {
-              // width: '150px',
-              height: "150px",
-              backgroundColor: "black",
+              height: "200px",
+              backgroundColor: "#3c3c3c",
               borderRadius: "10px",
             },
           },
           {
+            //rate
             type: FieldType.Rating,
+          },
+        ],
+      },
+      {
+        type: FieldType.Group,
+        fields: [
+          //line for Profile
+          {
+            type: FieldType.Line,
+            title: "Profile",
+          },
+          //// male | female
+          {
+            type: FieldType.Combo,
+            name: "gender",
+            title: "Gender",
+            async itemList() {
+              await sleep(1e3);
+              return [
+                "male-unique-key",
+                "female-unique-key",
+                "other-unique-key",
+              ];
+            },
+            async tr(current) {
+              await sleep(5e2);
+              if (current === "male-unique-key") {
+                return "Male";
+              } else if (current === "female-unique-key") {
+                return "Female";
+              } else if (current === "other-unique-key") {
+                return "Other";
+              } else {
+                return "";
+              }
+            },
+            defaultValue: "their-unique-key",
+          },
+          //however I don't understand how this will work
+          {
+            type: FieldType.Items,
+            title: "List",
+          },
+          // this div for keyyword db.json
+          {
+            type: FieldType.Div,
+            style: {
+              padding: "5px 50px",
+              display: "grid",
+              gridTemplateColumns: "auto 200px",
+            },
+
+            fields: [
+              {
+                type: FieldType.Text,
+                title: "keywords",
+                defaultValue: "keyword",
+                name: "keyword",
+              },
+              {
+                type: FieldType.Checkbox,
+                fieldBottomMargin: "0",
+                name: "subscribed",
+                title: "passphrase",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    type: FieldType.Div,
+    style: {
+      padding: "5px 50px",
+    },
+    fields: [
+      {
+        type: FieldType.Line,
+        title: "Common info",
+      },
+
+      //names
+      {
+        type: FieldType.Text,
+        title: "First name",
+        defaultValue: "Asadullo",
+        description: "Your first name",
+        name: "firstName",
+      },
+
+      //last name
+      {
+        type: FieldType.Text,
+        title: "Last name",
+        defaultValue: "Nazarov",
+        description: "Your last name",
+        name: "lastName",
+      },
+
+      //age
+      {
+        type: FieldType.Text,
+        inputType: "number",
+        title: "Age",
+        defaultValue: "21",
+        description: "47",
+        name: "age",
+      },
+
+      ///folowers
+
+      {
+        type: FieldType.Expansion,
+        title: "Folowers",
+        description: "notification subscription",
+        fields: [
+          {
+            type: FieldType.Text,
+            title: "phoneNumber",
+            description: "asadullon768@gmail.com",
+            name: "email",
+            defaultValue: true,
+          },
+          {
+            type: FieldType.Text,
+            title: "phoneNumber",
+            description: "+998909327526",
+            name: "phonenumber",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    type: FieldType.Div,
+    style: {
+      padding: "5px 50px",
+      display: "grid",
+      gridTemplateColumns: "auto auto",
+      gap: "5px",
+    },
+
+    fields: [
+      {
+        type: FieldType.Group,
+        fields: [
+          {
+            type: FieldType.Line,
+            title: "Work",
+          },
+          {
+            type: FieldType.Text,
+            title: "Job title",
+            defaultValue: "SMM",
+            name: "jobTitle",
+          },
+          {
+            type: FieldType.Text,
+            title: "place of work",
+            defaultValue: "SMM",
+            name: "jobArea",
           },
         ],
       },
@@ -50,21 +215,37 @@ const fields: TypedField[] = [
         fields: [
           {
             type: FieldType.Line,
-            title: "Profile",
+            title: "Home Adress",
           },
           {
-            type: FieldType.Items,
-            title: "Male",
-            itemList: ["Male", "Female"],
+            type: FieldType.Text,
+            title: "country",
+            defaultValue: "Uzb",
+            name: "country",
           },
           {
-            type: FieldType.Items,
-            title: "List",
+            type: FieldType.Text,
+            title: "city",
+            defaultValue: "Tashkent",
+            name: "city",
+          },
+          {
+            type: FieldType.Text,
+            title: "state",
+            defaultValue: "Andijan",
+            name: "state",
+          },
+          {
+            type: FieldType.Text,
+            title: "address",
+            defaultValue: "Yakkasaray district",
+            name: "address",
           },
         ],
       },
     ],
   },
+
   // {
   //   type: FieldType.Div,
   //   style: {
